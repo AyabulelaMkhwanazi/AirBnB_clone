92f716a1aab8319ea5a6439c807519ed3f0d3fd6
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2024-03-19 02:55:02 +0200

    check.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,4 +66,4 @@ class FileStorage:
                     del value["__class__"]
                     self.new(eval(class_name)(**value))
         except FileNotFoundError:
-            pass
+            return
